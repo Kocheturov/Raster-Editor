f8045c2e4e03a1467566170bac8e9c1da034d2f7
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 12:50:19 +0300

    Create palette.py

---
+++ Программа/palette.py
@@ -0,0 +1,12 @@
+class Palette:
+    def __init__(self, type):
+        self.type = type
+        # ...
+
+    def display_palette(self):
+        # Отображение палитры
+        # ...
+
+    def update_palette(self):
+        # Обновление палитры
+        # ...
