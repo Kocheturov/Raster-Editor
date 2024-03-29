6388c0bb670e17b5a82c81e3fbeafb39d3a5e896
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 15:23:54 +0300

    Update og_layers.py

--- Программа/og_layers.py
+++ Программа/og_layers.py
@@ -2,6 +2,16 @@ class LayerManager:
     # ... (методы create_layer, delete_layer, duplicate_layer)
 
     def move_layer(self, from_index, to_index):
+        """
+        Перемещает слой с одного индекса на другой.
+
+        Аргументы:
+            from_index (int): Индекс слоя, который нужно переместить.
+            to_index (int): Индекс, на который нужно переместить слой.
+
+        Вызывает:
+            ValueError: Если индексы слоев неверны.
+        """
         # Перемещение слоя
         if 0 <= from_index < len(self.layers) and 0 <= to_index < len(self.layers):
             layer = self.layers.pop(from_index)
@@ -10,5 +20,11 @@ class LayerManager:
             raise ValueError("Invalid layer index")
 
     def group_layers(self, layer_indices):
+        """
+        Группирует слои с указанными индексами.
+
+        Аргументы:
+            layer_indices (list): Список индексов слоев для группировки.
+        """
         # Группировка слоёв
         # ...
