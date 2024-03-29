3f83da7b0f61350572d347352f02aa0c474f0801
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 13:07:20 +0300

    Create texture.py

---
+++ Программа/texture.py
@@ -0,0 +1,10 @@
+class Filter:
+    # ... (метод apply_filter)
+
+    def distort_image(self, image, type, **kwargs):
+        # Применение искажения к изображению
+        # ...
+
+    def apply_texture(self, image, texture):
+        # Применение текстуры к изображению
+        # ...
