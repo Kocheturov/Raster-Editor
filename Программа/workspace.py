455f5312ad073ad3126fb701e98edda9b9b9f872
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 12:50:05 +0300

    Create workspace.py

---
+++ Программа/workspace.py
@@ -0,0 +1,12 @@
+class Workspace:
+    def __init__(self):
+        self.image = None
+
+    def display_image(self, image):
+        self.image = image
+        # Отображение изображения
+        # ...
+
+    def update_image(self):
+        # Обновление изображения после изменений
+        # ...
