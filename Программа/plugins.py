0004c264fa58f7e491b2800efcb28c290bb8d04d
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 13:09:28 +0300

    Create plugins.py

---
+++ Программа/plugins.py
@@ -0,0 +1,15 @@
+class PluginManager:
+    def __init__(self):
+        # ...
+
+    def load_plugins(self):
+        # Загрузка плагинов
+        # ...
+
+    def get_plugin(self, plugin_name):
+        # Получение плагина по имени
+        # ...
+
+    def run_plugin(self, plugin_name, image):
+        # Запуск плагина
+        # ...
