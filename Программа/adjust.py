3d73ba8c085510391061ecc044c2df4ff2c8de47
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 13:05:18 +0300

    Create adjust.py

---
+++ Программа/adjust.py
@@ -0,0 +1,14 @@
+class CorrectionTool:
+    # ... (метод apply_correction)
+
+    def adjust_levels(self, image, input_levels, output_levels):
+        # Настройка уровней изображения
+        # ...
+
+    def adjust_curves(self, image, curve):
+        # Настройка кривых изображения
+        # ...
+
+    def adjust_color_balance(self, image, shadows, midtones, highlights):
+        # Настройка цветового баланса изображения
+        # ...
