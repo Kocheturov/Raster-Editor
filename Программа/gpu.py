f4bf42c575970c81578b2477f0ad3d2580c1487a
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 15:20:13 +0300

    Update gpu.py

--- Программа/gpu.py
+++ Программа/gpu.py
@@ -2,5 +2,9 @@ class SystemManager:
     # ... (методы manage_memory, optimize_performance)
 
     def enable_gpu_acceleration(self):
+        """
+        Включает графическое ускорение, если оно доступно на 
+        аппаратном обеспечении.
+        """
         # Включение графического ускорения (если доступно)
         # ...
