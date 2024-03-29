3401b150b03e66387df5603bb991cb5fbba1c1c2
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 15:20:46 +0300

    Update help.py

--- Программа/help.py
+++ Программа/help.py
@@ -1,5 +1,13 @@
 class HelpSystem:
+    """
+    Класс представляет систему справки, предоставляющую информацию по 
+    различным темам.
+    """
+
     def __init__(self):
+        """
+        Инициализирует экземпляр HelpSystem со словарем тем справки.
+        """
         self.help_topics = {
             "tools": "Information about available tools",
             "layers": "How to work with layers",
@@ -7,6 +15,12 @@ class HelpSystem:
         }
 
     def display_help(self, topic):
+        """
+        Отображает справку по заданной теме.
+
+        Аргументы:
+            topic (str): Тема справки для отображения.
+        """
         # Отображение справки по заданной теме
         if topic in self.help_topics:
             print(self.help_topics[topic])
