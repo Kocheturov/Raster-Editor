6052cdd62a18e025fd54c93135af9f9138cc7f61
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 15:03:16 +0300

    Update add_edit_text.py

--- Программа/add_edit_text.py
+++ Программа/add_edit_text.py
@@ -1,19 +1,41 @@
 class TextTool:
+    """
+    Класс TextTool
+    """
     def __init__(self):
         self.active_text_box = None
 
     def add_text(self, text, position):
-        # Создание нового текстового блока
+        """Создание нового текстового блока
+
+        Args:
+          text: string.
+          position: int.
+    
+        Returns:
+          Text box.
+        """
         text_box = TextBox(text, position)
         self.active_text_box = text_box
         return text_box
 
     def edit_text(self, text):
-        # Редактирование активного текстового блока
+        """Редактирование активного текстового блока
+
+        Args:
+          text: string.
+    
+        """
+        # 
         if self.active_text_box:
             self.active_text_box.text = text
 
     def handle_input(self, event):
-        # Обработка ввода пользователя для редактирования текста
+        """Обработка ввода пользователя для редактирования текста
+
+        Args:
+          event: event.
+
+        """
         if self.active_text_box:
             # ...
