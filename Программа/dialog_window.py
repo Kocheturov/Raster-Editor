c424d68f27dc6178a1726b1e7a6e0fb58a1caf64
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 12:50:47 +0300

    Create dialog_window.py

---
+++ Программа/dialog_window.py
@@ -0,0 +1,12 @@
+class DialogWindow:
+    def __init__(self, type):
+        self.type = type
+        # ...
+
+    def display_dialog(self):
+        # Отображение диалогового окна
+        # ...
+
+    def handle_input(self):
+        # Обработка ввода пользователя
+        # ...
