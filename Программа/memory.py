4c7aa6b9931628d55607fa318b1cad23cd266108
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 13:11:36 +0300

    Create memory.py

---
+++ Программа/memory.py
@@ -0,0 +1,11 @@
+class SystemManager:
+    def __init__(self):
+        # ...
+
+    def manage_memory(self):
+        # Оптимизация использования памяти
+        # ...
+
+    def optimize_performance(self):
+        # Оптимизация производительности приложения
+        # ...
