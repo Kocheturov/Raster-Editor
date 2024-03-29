4bdca0cc394495dcf3c1818faa2e84937438a2e6
Kocheturov <99312630+Kocheturov@users.noreply.github.com>
2024-03-29 15:27:27 +0300

    Update toolbar.py

--- Программа/toolbar.py
+++ Программа/toolbar.py
@@ -1,5 +1,14 @@
 class Toolbar:
+    """
+    Класс представляет панель инструментов, отображающую кнопки 
+    инструментов и обрабатывающую их выбор.
+    """
+
     def __init__(self):
+        """
+        Инициализирует экземпляр Toolbar со словарем инструментов, 
+        содержащим информацию об иконках и действиях.
+        """
         self.tools = {
             "brush": {"icon": "brush.png", "action": self.activate_brush},
             "eraser": {"icon": "eraser.png", "action": self.activate_eraser},
@@ -7,11 +16,17 @@ class Toolbar:
         }
 
     def display_toolbar(self):
+        """
+        Отображает кнопки инструментов на экране.
+        """
         # Отображение кнопок инструментов
         for tool_name, data in self.tools.items():
             # ...
 
     def activate_brush(self):
+        """
+        Активирует инструмент "Кисть".
+        """
         # Активация инструмента "Кисть"
         # ...
 
